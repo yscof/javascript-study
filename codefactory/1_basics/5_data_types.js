be3c53a708406e0f2163debc3c9b7ef348180429
yscof <dexter1541@naver.com>
2026-04-12 00:24:41 +0900

    8. Data Types (데이터 타입)

--- codefactory/1_basics/5_data_types.js
+++ codefactory/1_basics/5_data_types.js
@@ -83,7 +83,7 @@ console.log(typeof isTrue);
 console.log(typeof isFalse);
 
 /**
- * Undefined
+ * undefined
  * 
  * 사용자가 직접 값을 초기화하지 않았을 때
  * 지정되는 값이다.
@@ -93,3 +93,88 @@ console.log(typeof isFalse);
 let noInit;
 console.log(noInit);
 console.log(typeof noInit);
+
+/**
+ * null 타입
+ * 
+ * undefined와 마찬가지로 값이 없다는 뜻이나
+ * JS에서는 개발자가 명시적으로 없는 값으로 초기화 할 때
+ * 사용된다.
+ */
+let init = null;
+console.log(init);
+console.log(typeof init);
+console.log('-------------');
+
+/**
+ * 
+ * Symbol 타입
+ * 
+ * 유일무이한 값을 생성할 때 사용한다.
+ * 다른 프리미티브 값들과 다르게 Symbol 함수를
+ * 호출해서 사용한다.
+ */
+const test1 = '1';
+const test2 = '1';
+
+console.log(test1 === test2);
+
+const symbol1 = Symbol('1');
+const symbol2 = Symbol('1');
+
+console.log(symbol1 === symbol2);
+
+/**
+ * Object 타입
+ * 
+ * Map
+ * 키: 밸류의 쌍으로 이루어져있다.
+ * key: value
+ */
+const dictionary = {
+    red: '빨간색', 
+    orange: '주황색', 
+    yellow: '노란색',
+};
+
+console.log(dictionary);
+console.log(dictionary['red']);
+console.log(dictionary['orange']);
+console.log(dictionary['yellow']);
+
+console.log(typeof dictionary);
+
+/**
+ * Array 타입
+ * 
+ * 값을 리스트로 나열할 수 있는 타입이다.
+ */
+const iveMembersArray = [
+    '안유진',
+    '가을',
+    '레이',
+    '장원영',
+    '리즈',
+    '이서',
+];
+console.log(iveMembersArray);
+
+/**
+ * index
+ * 
+ * 0부터 시작한다.
+ * 1씩 올라갑니다.
+ */
+console.log(iveMembersArray[0]);
+console.log(iveMembersArray[5]);
+
+iveMembersArray[0] = '코드팩토리';
+console.log(iveMembersArray);
+console.log(typeof iveMembersArray);
+
+/**
+ * static typing -> 변수를 선언할 때 어떤 타입의 변수를 선언할지 명시를 한다.
+ * 
+ * dynamic typing -> 변수의 타입을 명시적으로 선언하지 않고 값에 의해 타입을 "추론"한다.
+ * JS -> dynamic typing
+ */
